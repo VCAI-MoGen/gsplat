431bec9f844b82b6b9f9c6e6512880fcf5ec5513
PROLCY <ckd248@naver.com>
2025-09-28 11:57:15 +0000

    Add bindings

--- gsplat/cuda/csrc/rasterize_to_pixels_fwd_contrib.cu
+++ gsplat/cuda/csrc/rasterize_to_pixels_fwd_contrib.cu
@@ -302,7 +302,7 @@ std::tuple<torch::Tensor, torch::Tensor, torch::Tensor, torch::Tensor, torch::Te
 }
 
 std::tuple<torch::Tensor, torch::Tensor, torch::Tensor, torch::Tensor, torch::Tensor>
-rasterize_to_pixels_fwd_tensor_with_contrib(
+rasterize_to_pixels_fwd_with_contrib_tensor(
     // Gaussian parameters
     const torch::Tensor &means2d,   // [C, N, 2] or [nnz, 2]
     const torch::Tensor &conics,    // [C, N, 3] or [nnz, 3]
