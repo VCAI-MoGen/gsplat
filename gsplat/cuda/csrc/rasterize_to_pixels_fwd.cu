ddf7bb64081deec75bec4d3d9dcfd2d80fca89e0
PROLCY <ckd248@naver.com>
2025-09-28 10:54:03 +0000

    Fix error

--- gsplat/cuda/csrc/rasterize_to_pixels_fwd.cu
+++ gsplat/cuda/csrc/rasterize_to_pixels_fwd.cu
@@ -35,7 +35,7 @@ __global__ void rasterize_to_pixels_fwd_kernel(
     S *__restrict__ render_alphas, // [C, image_height, image_width, 1]
     int32_t *__restrict__ last_ids, // [C, image_height, image_width]
     bool *__restrict__ has_hit_any_pixels, // [C, N]
-    int32_t *__restrict__ n_contrib, // [C, image_height, image_width] # VCAI
+    int32_t *__restrict__ n_contrib // [C, image_height, image_width] # VCAI
 ) {
     // each thread draws one pixel, but also timeshares caching gaussians in a
     // shared tile
